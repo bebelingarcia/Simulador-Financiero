4c26e470321c7c53bd0266238eea4ac7df2c4385
bebelingarcia <gveo221448@upemor.edu.mx>
2024-11-24 21:55:39 -0600

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyOGNr6hktky+1bmqnWhENuQ",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
